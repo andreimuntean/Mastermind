daf79c06102bc757d7fc3d82f7582e50eb266072
Andrei Muntean <andrei.muntean.dev@gmail.com>
2015-07-09 14:03:09 +0300

    Created the project

---
+++ Mastermind/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Mastermind.Startup))]
+
+namespace Mastermind
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Mastermind
+[![Build Status](https://travis-ci.org/andreimuntean/Mastermind.svg?branch=master)](https://travis-ci.org/andreimuntean/Mastermind)
+
 Personal task manager.
